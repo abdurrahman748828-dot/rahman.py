da7135c1fdad8a31c7fb2fd627116c6b8cdc0ccb
abdurrahman748828-dot <abdurrahman748828@gmail.com>
2025-08-04 01:52:37 +0530

    Add files via upload

---
+++ rahman.py
@@ -0,0 +1,7 @@
+#/take two number one of them is 15
+#/another of them is of = 10
+
+print(15+10)   # Addition: 15 + 10 = 25
+print(15-10)   # Subtraction: 15 - 10 = 5
+print(15*10)   # Multiplication: 15 * 10 = 150
+print(15/10)   # Division: 15 / 10 = 1.5
